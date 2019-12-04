35ad7ec3c1db268ea31ce6a9bc934d436ee0fa46
Neil Stoker <neil.stoker@koiostechnology.co.uk>
2019-12-04 09:56:07 +0000

    Fixes from tutorial repo

--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -15,26 +15,27 @@ const (
 	port = ":50051"
 )
 
-type repository interface {
+// Repository holds the consignment data
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
-// Repository - Dummy repository simulating theuse of a datastore of some
+// ConsignmentRepository - Dummy repository simulating theuse of a datastore of some
 // kind. This will be replaced with a real implementation later.
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
 // Create a new consigment
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
 // GetAll consignments from the repository
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
@@ -43,7 +44,7 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures to
 // give you a better idea.
 type service struct {
-	repo         repository
+	repo         Repository
 	vesselClient vesselProto.VesselServiceClient
 }
 
@@ -74,7 +75,7 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 	return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
 	consignments := s.repo.GetAll()
 	res.Consignments = consignments
 	return nil
@@ -82,10 +83,11 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.Consignment, res 
 
 func main() {
 
-	repo := &Repository{}
+	repo := &ConsignmentRepository{}
 
 	srv := micro.NewService(
 		micro.Name("shippy.service.consignment"),
+		micro.Version("latest"),
 	)
 
 	srv.Init()
